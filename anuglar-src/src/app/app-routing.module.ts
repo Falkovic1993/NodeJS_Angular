41db0db9a182c30dea574753cb7250454e7ae883
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-02 00:08:28 +0200

    Added basic components + navbar style

---
+++ anuglar-src/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const appRoutes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'register', component: RegisterComponent},
+    {path:'login', component: LoginComponent},
+    {path:'dashboard', component: DashboardComponent},
+    {path:'profile', component: ProfileComponent},
+    
+
+]
+
+@NgModule({
+    imports: [
+      RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: false } // For debugging
+      )
+    ],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
--- anuglar-src/src/app/app.module.ts
+++ anuglar-src/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
