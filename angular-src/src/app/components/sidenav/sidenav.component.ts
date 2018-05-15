c62b87e7f567727e99d4d59b187b87cccc457eff
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-15 14:07:47 +0200

    Added name + mail in sidemenu

--- angular-src/src/app/components/sidenav/sidenav.component.ts
+++ angular-src/src/app/components/sidenav/sidenav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { myAuthService } from '../../services/auth.service';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from '../../store/store';
+import { User } from '../../entities/user';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,12 +10,18 @@ import { myAuthService } from '../../services/auth.service';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+ user: any;
 
   constructor(
     private authService:myAuthService,
+    private ngRedux: NgRedux<IAppState>,
   ) { }
 
   ngOnInit() {
+    this.user = localStorage.getItem('user');
+    console.log(this.user)
+    this.user = JSON.parse(this.user)
+    console.log(this.user)
   }
 
 }
