96aedb9591585e3aedc5e438250f0d9739ce123b
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-17 13:27:36 +0200

    Added new unit test to reducer (Redux)

--- angular-src/src/app/components/users-list/users-list.reducer.spec.ts
+++ angular-src/src/app/components/users-list/users-list.reducer.spec.ts
@@ -49,4 +49,34 @@ describe('Users Reducer', () => {
         //expect(newState).toEqual(1);
         expect(newState).toEqual(afterState);
     })
+
+    it('Should not delete anything if no user with id is found', ()=> {
+      const user = { 
+        id: 100,
+        firstname: 'Emil',
+        lastname: 'falk',
+        password: 'dasdas',
+        email: 'emil93falk@hotmail.com',
+        phone: 12121212
+      }
+      const user1 = { 
+        id: 122,
+        firstname: 'Emil',
+        lastname: 'falk',
+        password: 'dasdas',
+        email: 'emil93falk@hotmail.com',
+        phone: 12121212
+      }
+        let initialState = { usersState: undefined, users:[user,user1] };
+        deepFreeze(initialState);
+
+        let afterState = {  usersState: undefined, users:[user, user1] };
+
+        let newState = UsersReducer(initialState, {
+            type: types.UsersActions.USER_DELETED,
+            payload: 300
+        });
+        //expect(newState).toEqual(2);
+        expect(newState).toEqual(afterState);
+    })
 })
