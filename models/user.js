2198170ec9fd64c028d75afb3ebac95be8695b27
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-19 20:11:46 +0200

    db error test

--- models/user.js
+++ models/user.js
@@ -19,10 +19,15 @@ module.exports.addUser = function(newUser, callback){
 
 module.exports.getUserById = function(id, callback) {
 	console.log('DB',id);
-	let stmt = 'SELECT * FROM USERS WHERE id LIKE ?';
+	let stmt = 'SELECT * FROM USERS WHERE id = ?';
 	global.db.query(stmt, id, (err, user) => {
-		console.log('user get back' , user)
-		return callback(false, user);
+		if(err){
+			console.log(err)
+		} else {
+			console.log('user get back' , user)
+			return callback(false, user);
+
+		}
 	});
 };
 
