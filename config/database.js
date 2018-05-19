d53f65615d36202aaea7bd5a4e716894063bba8f
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-19 17:08:48 +0200

    Changed database server (FU one.com)

--- config/database.js
+++ config/database.js
@@ -1,20 +1,23 @@
 const mysql = require('mysql');
 
 global.db = mysql.createConnection({
-    host:'emilfk.dk.mysql',
-    user:"emilfk_dk",
-    password:"BenEtPjhKBUWzi46TSwTamoA",
-    database:"emilfk_dk"
-})
+	host:'mysql29.unoeuro.com',
+	user:'emilfk_dk',
+	password:'Falken11',
+	database:'emilfk_dk_db'
+});
+
+
 
 //mysql://b09cb87df8ff2a:61e9557f@us-cdbr-iron-east-04.cleardb.net/heroku_4d5dd86fbee2f56?reconnect=true
 db.connect( err => {
-    if(err){
-        console.log('Database error')
-        process.exit();
-    }
-    console.log('Connected to database')
-})
+	if(err){
+		console.log(err);
+		console.log('Database error');
+		process.exit();
+	}
+	console.log('Connected to database');
+});
 
 module.exports = {};
 
