4e50f9f13f60f1750f886a6ddd609460852b8817
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-19 12:30:23 +0200

    Test

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql');
 
 global.db = mysql.createConnection({
-    host:'mysql://b09cb87df8ff2a:61e9557f@us-cdbr-iron-east-04.cleardb.net/heroku_4d5dd86fbee2f56?reconnect=true',
-    user:"b49fd81ad844de",
-    password:"a30212fd",
+    host:'us-cdbr-iron-east-04.cleardb.net',
+    user:"b09cb87df8ff2a",
+    password:"61e9557f",
     database:"Nodejs_exam"
 })
 
+//mysql://b09cb87df8ff2a:61e9557f@us-cdbr-iron-east-04.cleardb.net/heroku_4d5dd86fbee2f56?reconnect=true
 db.connect( err => {
     if(err){
         console.log('Database error')
