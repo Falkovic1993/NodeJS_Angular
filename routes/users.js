1bf074a9f2016e1499664187ef3ef0c971a211a4
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-21 12:52:18 +0200

    Error in route

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ var multer = require('multer');
 
 
 const storage = multer.diskStorage({
-	destination: '../tmp/images/',
+	destination: '../tmp/images',
 	filename: function(req, file, cb){
 		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
 	}
