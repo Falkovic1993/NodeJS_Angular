9acfbced287becff4de0ab04d38dabdcbece9f18
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-05-22 09:40:15 +0200

    FIx for DB connection lost error

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql');
 
+/*
+
 global.db = mysql.createConnection({
 	host:'mysql29.unoeuro.com',
 	user:'emilfk_dk',
 	password:'Falken11',
 	database:'emilfk_dk_db'
 });
+
+
 
 
 
@@ -18,6 +22,42 @@ db.connect( err => {
 	}
 	console.log('Connected to database');
 });
+
+*/
+
+const db_config = {
+	host:'mysql29.unoeuro.com',
+	user:'emilfk_dk',
+	password:'Falken11',
+	database:'emilfk_dk_db'
+};
+
+
+
+let connection;
+
+function handleDisconnect() {
+	global.connection = mysql.createConnection(db_config); // Recreate the connection, since
+	// the old one cannot be reused.
+
+	connection.connect(function(err) {              // The server is either down
+		if(err) {                                     // or restarting (takes a while sometimes).
+			console.log('error when connecting to db:', err);
+			setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+		}                                     // to avoid a hot loop, and to allow our node script to
+	});                                     // process asynchronous requests in the meantime.
+	// If you're also serving http, display a 503 error.
+	connection.on('error', function(err) {
+		console.log('db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+			handleDisconnect();                         // lost due to either server restart, or a
+		} else {                                      // connnection idle timeout (the wait_timeout
+			throw err;                                  // server variable configures this)
+		}
+	});
+}
+
+handleDisconnect();
 
 module.exports = {};
 
