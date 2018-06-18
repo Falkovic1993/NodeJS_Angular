56c7f5d7c736b744be12fafcfa0ed33fec7c5b3a
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-06-18 15:01:18 +0200

    Added try catch to the project

--- models/user.js
+++ models/user.js
@@ -23,8 +23,9 @@ module.exports.getUserById = function(id, callback) {
 	global.db.query(stmt, id, (err, user) => {
 		if(err){
 			console.log(err);
+			return callback(false, err);
 		} else {
-			console.log('user get back' , user);
+		//	console.log('user get back' , user);
 			return callback(false, user);
 		}
 	});
@@ -32,17 +33,26 @@ module.exports.getUserById = function(id, callback) {
 
 module.exports.getUserByUserEmail = function(email, callback){
 	let stmt = 'SELECT * FROM users WHERE email LIKE ?';
-	global.db.query(stmt, email, (err, user) => {
-		return callback(false, user[0]);
-	}); 
+	try{
+		global.db.query(stmt, email, (err, user) => {		
+			return callback(false, user[0]);
+		}); 
+	}
+	catch(err){
+		console.log(err, 'could not get user by mail');
+	}
 };
 
 module.exports.activateUserAccount = function(mail, callback){
 	let stmt = 'UPDATE users SET isActive = ? WHERE email LIKE ?';
 	let data = [true, mail];
-	global.db.query(stmt, data, (err, user) => {
-		//console.log('USER INFO',user);
-	}); 
+	try{
+		global.db.query(stmt, data, (err, user) => {
+			//console.log('USER INFO',user);
+		}); 
+	}catch(err){
+		console.log(err, 'Error on activate account');
+	}
 };
 
 module.exports.updateUser = function(newUser, callback){
@@ -53,10 +63,16 @@ module.exports.updateUser = function(newUser, callback){
 			newUser.password = hash;
 			let jUser = [newUser, newUser.id];
 			let stmt = 'UPDATE users SET ? WHERE id = ?';
-			global.db.query(stmt, jUser, (err, user) => {
+			try{
+				global.db.query(stmt, jUser, (err, user) => {
+					console.log(user, 'Has been updated');
+				});}
+			catch(err){
+				console.log(err, 'Something went wrong!');
+			}
 			//console.log(user);
 
-			});
+			
 		});
 	});
 };
@@ -71,16 +87,26 @@ module.exports.comparePassword = function(TypedPassword, hash, callback){
 
 module.exports.getAllUsers = function(callback){
 	let stmt = 'SELECT * FROM users';
-	global.db.query(stmt,  (err, users) => {
-		//console.log(users);
-		return callback(false, users);
-	}); 
+	try{
+		global.db.query(stmt,  (err, users) => {
+			//console.log(users);
+			return callback(false, users);
+		}); 
+	}catch(ex){
+		console.log(ex, 'Could not get all users');
+		return callback(true);
+	}
 };
 
 module.exports.deleteUser = function(id, callback){
 	const userId = id;
 	let stmt = 'DELETE FROM users WHERE id = ?';
-	global.db.query(stmt, userId, (err, data) => {
-		return callback(false, data);
-	});
+	try{
+		global.db.query(stmt, userId, (err, data) => {
+			return callback(false, data);
+		});
+	}catch(ex){
+		console.log(ex, 'Error deleting user');
+		return callback(true, null);
+	}
 };
