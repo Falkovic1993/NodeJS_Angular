eff4dd07297dac0d60db1b70296a6dd1711f3085
Emil Falk Knudsen <emilfalkknudsen93@gmail.com>
2018-06-18 15:02:45 +0200

    Some updated tests

--- angular-src/e2e/standardfunc.e2e-spec.ts
+++ angular-src/e2e/standardfunc.e2e-spec.ts
@@ -4,38 +4,39 @@ import { protractor } from 'protractor/built/ptor';
 
 describe('Test standard user functions as register, login, logout', () => {
   it('1.0 - Register a new user', async() => {
+    browser.driver.manage().window().maximize();
     browser.get('/register');
     element(by.id('firstname')).sendKeys('Emil');
       element(by.id('lastname')).sendKeys('Falk');
-      element(by.id('password')).sendKeys('eeewww');
-      element(by.id('password2')).sendKeys('eeewww');
+      element(by.id('password')).sendKeys('ee');
+      element(by.id('password2')).sendKeys('ee');
       element(by.id('email')).sendKeys('emik@html24.net');
       element(by.id('phone')).sendKeys('12345678');
       element(by.id('btnSignUp')).click();
       const EC = protractor.ExpectedConditions;
-      browser.wait(EC.urlContains('http://localhost:49152/login'), 1000).then(function(result) {
+      browser.wait(EC.urlContains('http://localhost:49152/login'), 3000).then(function(result) {
         expect(result).toEqual(true);
       })
   })
   it('1.1 - Log into the site', async() => {
     browser.get('/login');
-    element(by.id('password')).sendKeys('eeqq');
-    element(by.id('email')).sendKeys('emil93falk@hotmail.com');
+    element(by.id('password')).sendKeys('ee');
+    element(by.id('email')).sendKeys('emik@html24.net');
     element(by.id('btnLogin')).click();
       const EC = protractor.ExpectedConditions;
-      browser.wait(EC.urlContains('http://localhost:49152/dashboard'), 1000).then(function(result) {
+      browser.wait(EC.urlContains('http://localhost:49152/dashboard'), 3000).then(function(result) {
         expect(result).toEqual(true);
       })
   })
   it('1.2 - Log out of the site', async() => {
     browser.get('/login');
-    element(by.id('password')).sendKeys('eeqq');
-    element(by.id('email')).sendKeys('emil93falk@hotmail.com');
+    element(by.id('password')).sendKeys('ee');
+    element(by.id('email')).sendKeys('emik@html24.net');
     element(by.id('btnLogin')).click();
       const EC = protractor.ExpectedConditions;
-      browser.wait(EC.urlContains('http://localhost:49152/dashboard'), 1000).then(function(result) {
+      browser.wait(EC.urlContains('http://localhost:49152/dashboard'), 3000).then(function(result) {
         element(by.id('btnLogout')).click();
-        browser.wait(EC.urlContains('http://localhost:49152/home'), 1000).then(function(result) {
+        browser.wait(EC.urlContains('http://localhost:49152/home'), 3000).then(function(result) {
         expect(result).toEqual(true);
       })
       })
